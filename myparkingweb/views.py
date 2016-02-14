c2b973e61292b0a6c8b8778e9306796b1c1923db
miroslav <trollologic@gmail.com>
2016-02-14 20:12:54 +0100

    fixed issue with adding author in post

--- myparkingweb/views.py
+++ myparkingweb/views.py
@@ -14,7 +14,7 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            # post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return render(request, 'myparkingweb/post_edit.html', {'form': form,})
